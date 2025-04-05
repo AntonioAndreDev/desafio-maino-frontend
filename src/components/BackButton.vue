ed19f52553617f9294745206ea194d6d5c0f81c5
Antonio Andre <antonioandre1008@gmail.com>
2025-04-05 15:12:20 -0500

    :sparkles: feat: cria componente botão de voltar

---
+++ src/components/BackButton.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="flex items-center">
+        <button @click="router.back()"
+                class="bg-indigo-600/50 hover:bg-indigo-600 md:px-4 md:py-2 px-2 py-1 rounded-lg text-sm transition-colors cursor-pointer flex items-center gap-x-2 font-medium"
+        >
+            <Icon class="size-5" icon="carbon:return"/>
+            Voltar
+        </button>
+    </div>
+</template>
+
+<script setup>
+import {useRouter} from "vue-router";
+import {Icon} from "@iconify/vue";
+
+const router = useRouter();
+</script>
