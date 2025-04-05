2185c272642e0d2ccef6d7adb06a9121510555e5
Antonio Andre <antonioandre1008@gmail.com>
2025-04-05 00:24:33 -0500

    :wrench: chore: instala e configura vue debounce

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
+import vueDebounce from "vue-debounce";
 
 const app = createApp(App)
 
 app.use(router)
+app.directive('debounce', vueDebounce())
 
 app.mount('#app')
