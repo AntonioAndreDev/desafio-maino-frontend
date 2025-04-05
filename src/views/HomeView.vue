78e1d6cb07d63c76383252bef61f5cdd4931d1da
Antonio Andre <antonioandre1008@gmail.com>
2025-04-04 19:42:44 -0500

    :wrench: chore: instala e configura tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <h1 class="text-3xl">Hello Mainô!</h1>
+    </main>
 </template>
