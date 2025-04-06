0dac7a1441cdd47c59cd61f330c26edb89857cf4
Antonio Andre <antonioandre1008@gmail.com>
2025-04-06 17:48:43 -0500

    :sparkles: feat: padroniza select de mudança de idioma

--- src/components/home/TheHeader.vue
+++ src/components/home/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-between items-center">
+  <div class="flex md:flex-row flex-col gap-4 justify-between items-center">
     <RouterLink :to="{ name: 'home' }">
       <h1
         class="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500 mb-4 w-fit"
@@ -8,7 +8,7 @@
       </h1>
     </RouterLink>
 
-    <ChangeIdiom />
+    <ChangeIdiom class="mb-6" />
   </div>
 
   <div class="grid md:grid-cols-3 gap-4 mb-4 items-center">
